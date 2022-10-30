df861f15c9d46d41509d2204c7fbcbf6bf6d8a3c
Ankit Maurya <am8365965@gmail.com>
2022-10-30 17:04:08 +0530

    display changed to flex from grid

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,10 +10,10 @@ const Hero = () => {
                 {/* Left Side */}
                 <div className='left'>
                     <p>Cryptocurrency tracker allows you to track the Current value of Cryptocurrencies </p>
-                    <h1>Connect your Crypto Wallet with our Website</h1>
+                    <h1>Experience Realtime Crypto Tracking </h1>
                     <p>Buy, Sell, and store hundreds of cryptocurrencies</p>
                     <div className='input-container'>
-                        <button className='btn'>Learn More</button>
+                        <button className='btn'>Current Prices</button>
                     </div>
                 </div>
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Signup.css'
 
-import Crypto from '../assets/trade.png'
+
 
 const Signup = () => {
     return (
@@ -14,11 +14,11 @@ const Signup = () => {
 
                 {/* right */}
                 <div className='right'>
-                    <h2>Earn passive income with crypto.</h2>
-                    <p>Earn up to 12% annual rewards on 30+ digital assets. Simply hold your assets in the app to automatically earn rewards at the end of each month with no lockups and no limits.</p>
+                    <h2>Contact Us </h2>
+                    <p>COMPLAINTS: you can reach out to us for any type of complaints or for any type of queries related to our website SUGGESTIONS: any suggestions from your side are most welcomed</p>
                     <div className='input-container'>
                         <input type='email' placeholder='Enter your email' />
-                        <button className='btn'>Learn More</button>
+                        <button className='btn'>Submit</button>
                     </div>
                 </div>
 
